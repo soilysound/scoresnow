ac439f81dedc504e6448c0948b6ff187b334b56a
soilysound <soilysound@gmail.com>
2014-04-15 22:35:32 +0100

    add match view

--- js/modules/render-functions/football-match.js
+++ js/modules/render-functions/football-match.js
@@ -6,4 +6,18 @@ module.exports = function(data){
 
   var isDefinedLeague = SCORESNOW.competitionLookup[SCORESNOW.currentSport][data.competitionId];
 
+  updateText(this.querySelector('.data-bar__cell-hometeam'), data.teams.home.name);
+  updateText(this.querySelector('.data-bar__cell-awayteam'), data.teams.away.name);
+
+  updateText(this.querySelector('.data-bar__cell-homescore'), data.score.home);
+  updateText(this.querySelector('.data-bar__cell-awayscore'), data.score.away);
+
+  var status = SCORESNOW.statusLookup[data.generalInfo.matchStatus.toLowerCase()];
+  this.querySelectorAll('.data-bar').forEach(function(item){
+    item.setAttribute('data-status', status);
+  });
+
+
+
+
 };
